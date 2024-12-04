39034d956c5a0f59c10cbc81fc0051e8a877ff34
Tanmay Jain <tanmayjain101011200@gmail.com>
2024-12-04 22:48:55 +0530

    ADD FILES

---
+++ BasicReact/src/Title.jsx
@@ -0,0 +1,11 @@
+function Title(){
+    return (
+        <div>
+     <h1>This is my app component</h1>
+      <p>The component inside we have</p>
+    <h1>I am the Title!</h1>
+    </div>
+);
+  };
+
+  export default Title;
